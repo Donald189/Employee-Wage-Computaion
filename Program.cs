258ebcef33047f4b65bb820e09fe50fc4df2e3b2
Donald189 <donaldkarey@gmail.com>
2022-02-21 10:02:31 +0530

    Employee Wage Computation

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+            //EmployeeAttendance.PresentorAbsent();
+
+            //EmployeeDailyWage.CalEmployee();
+
+            //EmpSwitchCase.CalEmployeeSwitch();
+
+            //EmpWagePerMonth.CalEmployeeWagePerMonth();
+
+            //EmpMonthlyWageCalc.EmpWageCalcWithHoursOrDays();
+            
+        }
+    }
+}
